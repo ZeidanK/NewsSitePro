d38582adb8f36ba91e3ffb3830d907595c691e94
Karim Zeidan <karemziedan@gmail.com>
2025-06-21 16:27:25 +0300

    login with token to local storage works same with reg

--- BL/Requests.cs
+++ BL/Requests.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NewsSite.Models
 {
     public class LoginRequest
@@ -8,8 +10,15 @@ namespace NewsSite.Models
 
     public class RegisterRequest
     {
+        [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string Password { get; set; }
     }
 
